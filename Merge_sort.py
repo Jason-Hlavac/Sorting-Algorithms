9592eb0eda0508736747dd9ec8fe2b306c435dcc
Jason-Hlavac <49419324+Jason-Hlavac@users.noreply.github.com>
2019-04-10 20:00:05 -0700

    Update Merge_sort.py

--- Merge_sort.py
+++ Merge_sort.py
@@ -7,27 +7,27 @@ for i in range(random.randint(75,150)):
 print (numbers)    
 def merge_sort(list):
     if len(list) > 1:
-        mid = round(len(list)//2)
+        mid = (len(list)//2)
         l = list[mid:]
         r = list[:mid]
         merge_sort(l)
         merge_sort(r)
         i = j = k = 0
-        while (i < len(l)-1 and j < len(r)-1):
+        while (i < len(l) and j < len(r)):
             if (l[i] < r[j]):
-                list[k] = l[j]
+                list[k] = l[i]
                 i+=1
             else:
                 list[k] = r[j]
                 j+=1
             k+=1
             
-        while i < len(l)-1: 
+        while i < len(l): 
             list[k] = l[i] 
             i+=1
             k+=1
           
-        while j < len(r)-1: 
+        while j < len(r): 
             list[k] = r[j] 
             j+=1
             k+=1
